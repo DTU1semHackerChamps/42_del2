7739b2dd3393f69cc28254962aeec4dde0f0af6c
Djuncle <djuncledk@gmail.com>
2020-10-26 18:40:37 +0100

    Added tile constructor

--- TileGame/src/Tile.java
+++ TileGame/src/Tile.java
@@ -1,7 +1,29 @@
-public class Tile {
+public class Tile
+{
     private int balanceChange;
     private boolean extraTurn;
 
-    public Tile()
+    public Tile(int a, boolean b)
+    {
+        balanceChange = a;
+        extraTurn = b;
 
+
+    }
+
+    public void setBalanceChange(int balanceChange) {
+        this.balanceChange = balanceChange;
+    }
+
+    public void setExtraTurn(boolean extraTurn) {
+        this.extraTurn = extraTurn;
+    }
+
+    public int getBalanceChange() {
+        return balanceChange;
+    }
+
+    public boolean isExtraTurn() {
+        return extraTurn;
+    }
 }
