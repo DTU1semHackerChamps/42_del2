6c2081cdb794623ab165ccc986f0a0fb42b85ebd
Spastinak <71653506+Spastinak@users.noreply.github.com>
2020-10-26 18:56:59 +0100

    Added array of tile list

--- TileGame/src/Tile.java
+++ TileGame/src/Tile.java
@@ -1,5 +1,7 @@
-public class Tile
-{
+public class Tile {
+    /**
+     * constructor
+     */
     private int balanceChange;
     private boolean extraTurn;
 
@@ -7,7 +9,6 @@ public class Tile
     {
         balanceChange = a;
         extraTurn = b;
-
 
     }
 
@@ -25,5 +26,41 @@ public class Tile
 
     public boolean isExtraTurn() {
         return extraTurn;
+    }
+
+
+    /**
+     * array of tile list
+     * @return returns array of the specified initiated tiles
+     */
+    public static Tile [] tileListInit(){
+        Tile [] Tiles = new Tile[12];
+        Tile Start = new Tile (0,false);
+        Tile Tower = new Tile (250,false);
+        Tile Crater = new Tile (-100,false);
+        Tile PalaceGates = new Tile (100,false);
+        Tile ColdDesert = new Tile (-20,false);
+        Tile WalledCity = new Tile (180,false);
+        Tile Monastery = new Tile (0,false);
+        Tile BlackCave = new Tile (-70,false);
+        Tile HutsInTheMountain = new Tile (60,false);
+        Tile TheWerewall = new Tile (-80,true);
+        Tile ThePit = new Tile (-50,false);
+        Tile Goldmine = new Tile (650,false);
+
+        Tiles[0] = Start;
+        Tiles[1] = Tower;
+        Tiles[2] = Crater;
+        Tiles[3] = PalaceGates;
+        Tiles[4] = ColdDesert;
+        Tiles[5] = WalledCity;
+        Tiles[6] = Monastery;
+        Tiles[7] = BlackCave;
+        Tiles[8] = HutsInTheMountain;
+        Tiles[9] = TheWerewall;
+        Tiles[10] = ThePit;
+        Tiles[11] = Goldmine;
+
+        return Tiles;
     }
 }
