cf934a2f4bd09c1c8eea41cef2cf3b76c47fc39c
Zahed <zahedm45@gmail.com>
2020-10-26 23:27:23 +0100

    Change occurred.

--- TileGame/src/Dice.java
+++ TileGame/src/Dice.java
@@ -1,4 +1,18 @@
-public class Dice {
 
+public class Dice {
+    private int faceValue;
+
+   public int rollDice () {
+       faceValue =(int) (Math.random()*6)+1;
+       return faceValue;
+   }
+
+    public int getFaceValue() {
+        return faceValue;
+    }
+
+    public void setFaceValue(int faceValue) {
+        this.faceValue = faceValue;
+    }
 
 }
