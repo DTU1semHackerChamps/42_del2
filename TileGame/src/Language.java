6a9d8288c21d5ad7912d260dfee55807d4dd03d2
Emil T <emilbfh@hotmail.com>
2020-10-27 17:43:03 +0100

    Added Language.java for easy translation

---
+++ TileGame/src/Language.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.io.BufferedReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+public class Language {
+
+    public static HashMap<String, String> languageInit() throws IOException {
+        Path path = Paths.get("Dansk.txt");
+        BufferedReader reader = new BufferedReader(new FileReader(String.valueOf(path.toAbsolutePath())));
+
+        HashMap<String, String> langList = new HashMap<>();
+
+        for (int i = 0; i < 2; i++) {
+            langList.put(reader.readLine(), reader.readLine()); // langList.put(keyWord, Data)
+
+        }
+        return langList;
+    }
+
+}
--- TileGame/src/Main.java
+++ TileGame/src/Main.java
@@ -1,5 +1,14 @@
+import java.io.IOException;
+import java.util.HashMap;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        HashMap<String, String> stringList;
+
+        stringList = Language.languageInit();
+
+        System.out.println(stringList.get("vundetBesked"));
+        System.out.println(stringList.get("kastBesked"));
 
     }
 }
