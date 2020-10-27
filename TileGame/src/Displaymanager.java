6b54e391bf57521a869c51dbdacae31775bec45c
Machnuud <Albert88->
2020-10-27 18:48:42 +0100

    Methods added to displaymanager class

--- TileGame/src/Displaymanager.java
+++ TileGame/src/Displaymanager.java
@@ -1,3 +1,30 @@
 public class Displaymanager {
+    public static void startscreen(){
+
+    }
+
+    public static void displayDice(){
+
+    }
+
+    public static void initBoard(){
+
+    }
+
+    public static void winScreen(){
+
+    }
+
+    public static void displayBalance(){
+
+    }
+
+    public static void displayPosition(){
+
+    }
+
+    public static void displayTileText(){
+
+    }
 
 }
