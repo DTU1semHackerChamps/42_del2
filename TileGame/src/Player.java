e356dfca00867c1b08ca7448537d3a8ffc12fbbd
Spastinak <71653506+Spastinak@users.noreply.github.com>
2020-10-27 17:44:20 +0100

    added constroctor

--- TileGame/src/Player.java
+++ TileGame/src/Player.java
@@ -1,2 +1,28 @@
 public class Player {
+    private int balance;
+    private int position;
+    private boolean playerTurn;
+    private boolean extraTurn;
+    private boolean hasWon;
+
+
+    public Player(int a, int b, boolean c, boolean d, boolean e){
+
+        balance = a;
+        position = b;
+        playerTurn = c;
+        extraTurn = d;
+        hasWon = e;
+
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+
 }
