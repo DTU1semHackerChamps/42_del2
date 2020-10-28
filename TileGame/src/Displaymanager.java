a84e11b919426fca124ccd45e8bb9b7dec7b1356
Zahed <zahedm45@gmail.com>
2020-10-28 16:44:42 +0100

    Temporary commit.

--- TileGame/src/Displaymanager.java
+++ TileGame/src/Displaymanager.java
@@ -4,25 +4,18 @@ import gui_fields.GUI_Field;
 import java.io.IOException;
 import java.util.HashMap;
 public class Displaymanager {
-//    throws IOException;
     GUI gui = new GUI();
     Dice a = new Dice(3);
 
-//    HashMap<String, String> stringList;
-//    stringList = Language.languageInit();
-
 
     public void startScreen(){
-        //gui.getUserButtonPressed("Press OK to roll the dice ");
         gui.showMessage("Press OK to roll the dice ");
-        //return startScreen();
+
     }
     public void startButton(){
         while (gui.getUserLeftButtonPressed(" ","Roll","Exit")){
             gui.setDice(a.rollDice(),a.rollDice());
         }
-
-
     }
 
     public void displayDice(){
