724f789355b1c256dd0a2f7340f3f17569e09431
Chris2670 <70951742+Chris2670@users.noreply.github.com>
2020-10-29 16:54:35 +0100

    DiceTest and TileTest classes added

---
+++ TileGame/src/DiceTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiceTest {
+
+    @org.junit.jupiter.api.Test
+    void rollDice() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFaceValue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setFaceValue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
---
+++ TileGame/src/TileTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TileTest {
+
+    @Test
+    void setBalanceChange() {
+    }
+
+    @Test
+    void setExtraTurn() {
+    }
+
+    @Test
+    void getBalanceChange() {
+    }
+
+    @Test
+    void isExtraTurn() {
+    }
+
+    @Test
+    void tileListInit() {
+    }
+}
