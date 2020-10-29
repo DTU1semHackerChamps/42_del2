6fb22ab98eba07583b19a958868655e96581e274
Chris2670 <70951742+Chris2670@users.noreply.github.com>
2020-10-29 19:01:40 +0100

    Added tests to DiceTest and TileTest

--- TileGame/src/TileTest.java
+++ TileGame/src/TileTest.java
@@ -1,26 +1,14 @@
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-class TileTest {
+    public class TileTest {
+        // tests if the program correctly adds or subtracts the correct amount of points and if the extraturn rule works
+        @org.junit.jupiter.api.Test
+        void tileListInit() {
+            Tile [] tilesTest = new Tile[12];
+            tilesTest = Tile.tileListInit();
+            assertEquals(250,tilesTest[1].getBalanceChange());
+            assertEquals(-100,tilesTest[2].getBalanceChange());
+            assertTrue(tilesTest[9].isExtraTurn());
 
-    @Test
-    void setBalanceChange() {
+        }
     }
-
-    @Test
-    void setExtraTurn() {
-    }
-
-    @Test
-    void getBalanceChange() {
-    }
-
-    @Test
-    void isExtraTurn() {
-    }
-
-    @Test
-    void tileListInit() {
-    }
-}
